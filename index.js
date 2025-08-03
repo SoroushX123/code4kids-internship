fd9965edb9a012e0449bd3f964abd9d9596f7f97
soroush iliyat <soroushiliyat@yahoo.com>
2025-08-03 23:46:35 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("Thank you for contacting Code4Kids! We’ll get back to you soon.");
 });
+
+document
+  .querySelector("#volunteerForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert(
+      "Thank you for applying as a volunteer! We'll review your application soon."
+    );
+  });
